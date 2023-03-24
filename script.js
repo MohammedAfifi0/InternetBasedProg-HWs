9d1b93d41e7a0f011861d1503ff6f317b30eb39d
Afify <95406432+MohammedAfifi0@users.noreply.github.com>
2023-03-24 03:17:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function callMe() {
+    window.location.href = "tel:+905524409212";
+  }
+  
+  function emailMe() {
+    window.location.href = "mailto:support-in@google.com?subject=Hello%20Google!!";
+  }
+  
+  function RunJs() {
+    alert("Runs JavaScript!");
+  }
+  
